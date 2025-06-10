407584d86018202fb48ca12b0b6afcf98d668409
YuWonJong <wonbell00@gmail.com>
2025-06-10 23:43:53 +0900

    servlet

--- src/main/java/hello/servlet/web/servlet/MemberSaveServlet.java
+++ src/main/java/hello/servlet/web/servlet/MemberSaveServlet.java
@@ -19,6 +19,8 @@ public class MemberSaveServlet extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("MemberSaveServlet.service");
         String username = request.getParameter("username");
+
+
         int age = Integer.parseInt(request.getParameter("age"));
 
         Member member = new Member(username, age);
