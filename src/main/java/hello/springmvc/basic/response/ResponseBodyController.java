6c9c54d13f969817e81392f2c8dc39dfd3910279
YuWonJong00 <wonbell00@gmail.com>
2025-06-26 23:27:44 +0900

    merge

--- src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -14,10 +14,7 @@ import java.io.IOException;
 
 @Slf4j
 @Controller
-<<<<<<< HEAD
-=======
 
->>>>>>> c67440d (RequestBody)
 public class ResponseBodyController {
     @GetMapping("/response-body-v1")
     public void responseBodyV1(HttpServletResponse response) throws IOException {
