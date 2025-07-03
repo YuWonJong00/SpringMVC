19ad917b8ac51a207d9f4b19b2bc8eefbabb37a3
YuWonJong00 <wonbell00@gmail.com>
2025-07-03 21:45:20 +0900

    itemAddForm

--- src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/item/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -14,11 +15,23 @@ import java.util.List;
 @RequestMapping("/basic/items")
 @RequiredArgsConstructor
 public class BasicItemController {
-private final ItemRepository itemRepository;
-@GetMapping
+    private final ItemRepository itemRepository;
+
+    @GetMapping
     public String items(Model model) {
-    List<Item> items = itemRepository.findAll();
-    model.addAttribute("items", items);
-    return "basic/items";
-}
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable("itemId")Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
 }
